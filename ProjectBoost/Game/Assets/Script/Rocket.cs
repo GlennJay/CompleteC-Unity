31e9cc28aac8c23188476efd515c962c2ba988af
unknown <glennwjason@gmail.com>
2020-02-08 23:37:00 -0500

    added the thrust to the rocket

---
+++ ProjectBoost/Game/Assets/Script/Rocket.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>(); //rigidbody of the rocket ship
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if(Input.GetKey(KeyCode.Space)){ //can thrust while rotating
+            print("thrust");
+            Thrust();
+        }
+
+        if(Input.GetKey(KeyCode.A)){ //rotate right
+            print("rotate right");
+        }else if(Input.GetKey(KeyCode.D)){ //rotate left
+            print("roate left");
+        }
+    }
+
+    private void Thrust()
+    {
+        rigidBody.AddRelativeForce(Vector3.up); //relative force allows the changes to change on local scope in game
+    }
+}
