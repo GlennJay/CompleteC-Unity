121660970b25c9c75290a51311fdeb36e9c3f702
unknown <glennwjason@gmail.com>
2020-02-09 11:38:37 -0500

    Added sound to game object and script

--- ProjectBoost/Game/Assets/Script/Rocket.cs
+++ ProjectBoost/Game/Assets/Script/Rocket.cs
@@ -6,10 +6,14 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
+     AudioSource audioSource;
+    
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>(); //rigidbody of the rocket ship
+        audioSource = GetComponent<AudioSource>(); //audio for thrust
+        
     }
 
     // Update is called once per frame
@@ -21,19 +25,26 @@ public class Rocket : MonoBehaviour
     private void ProcessInput()
     {
         if(Input.GetKey(KeyCode.Space)){ //can thrust while rotating
-            print("thrust");
+        
             Thrust();
+        }else{
+            audioSource.Stop(); //stop audio based of space key
         }
 
         if(Input.GetKey(KeyCode.A)){ //rotate right
-            print("rotate right");
+            transform.Rotate(Vector3.forward);
         }else if(Input.GetKey(KeyCode.D)){ //rotate left
-            print("roate left");
+            transform.Rotate(Vector3.back);
         }
     }
 
     private void Thrust()
     {
         rigidBody.AddRelativeForce(Vector3.up); //relative force allows the changes to change on local scope in game
+      
+         if(!audioSource.isPlaying){
+            audioSource.Play();
+       }
+
     }
 }
