e70af529140a968ee6108627a04a3c632a7bc3e7
GlennJay <glennwjason@gmail.com>
2020-02-25 12:31:03 -0500

    working on moving platforms

---
+++ ProjectBoost/Game/Assets/Script/Oscillator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour //for moving platforms
+{
+
+[SerializeField] Vector3 movementVector; //where the object will move
+
+//TODO remove from inspector
+[Range(0,1)][SerializeField]float movementFactor; // 0 for not moved, 1 for fully moved
+
+Vector3 startPos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;//get starting position of the game object
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 offSet = movementFactor * movementVector;
+        transform.position = startPos + offSet;
+        
+    }
+}
