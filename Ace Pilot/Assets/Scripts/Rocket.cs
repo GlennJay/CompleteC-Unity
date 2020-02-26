7711743a3e05062f8f84cadbbb490da1f7c11439
GlennJay <glennwjason@gmail.com>
2020-02-26 14:35:21 -0500

    Doing thrust

---
+++ Ace Pilot/Assets/Scripts/Rocket.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Rocket : MonoBehaviour
+{
+    public float mainThrust = 1f;
+     Rigidbody rigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputProcess();
+
+    }
+
+    private static void InputProcess()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            print("right");
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            print("left");
+        }
+    }
+}
