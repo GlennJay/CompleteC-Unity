578563894d99427d5a8c793b1f6fdef34a759ba2
GlennJay <glennwjason@gmail.com>
2020-02-28 08:30:01 -0500

    added tags, audio clips

--- Ace Pilot/Assets/Scripts/Rocket.cs
+++ Ace Pilot/Assets/Scripts/Rocket.cs
@@ -9,38 +9,83 @@ public class Rocket : MonoBehaviour
     [SerializeField] float mainThrust;
     //rotation thrust make serialized
     [SerializeField] float rotationThrust;
-    
+    AudioSource audiosource;
+    [SerializeField] AudioClip engine;
+    [SerializeField] AudioClip explosion;
+    [SerializeField] AudioClip success;
 
-
-
-
+    enum State { Alive, Dead, Transcending};
+    State state = State.Alive;
      Rigidbody rigidBody;
      
     // Start is called before the first frame update
     void Start()
     {
+         
         rigidBody = GetComponent<Rigidbody>();
-        
+        audiosource = GetComponent<AudioSource>();
+        audiosource.clip = success;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Rotation();
-        Thrust();
+        if(state == State.Alive)
+        {
+            Rotation();
+            respondToThrustInput();
+        }
+       
 
     }
+
+    //FUNCTION detect collisions, enemy collision trigers death, friendly collision trigers transcending
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "enemy":
+                audiosource.Stop();
+                audiosource.PlayOneShot(explosion);
+                break;
+            case "success":
+                audiosource.Stop();
+                audiosource.PlayOneShot(success);
+                break;
+            default:
+                break;
+        }
+    }
+
+    //FUNCTION respond to thrust input
+    void respondToThrustInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Thrust();
+        }
+        else
+        {
+            audiosource.Stop();
+
+        }
+
+    }
+
+
 
     //FUNCTION thust
     //process the thrust make sure to make as frame rate independent
     void Thrust()
     {
-        float frameThrust = mainThrust * Time.deltaTime;
-        if (Input.GetKey(KeyCode.Space))
+        rigidBody.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+        if (!audiosource.isPlaying)
         {
-            rigidBody.AddRelativeForce(Vector3.up * frameThrust);
-            
+            audiosource.PlayOneShot(engine);
         }
+        
+
     }
 
 
