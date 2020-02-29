939966047e82feb5ab6b4f27984c79ce2d5f6d2f
GlennJay <glennwjason@gmail.com>
2020-02-28 21:18:01 -0500

    added follow camera

---
+++ Ace Pilot/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    //Player object to follow
+    public Transform targetPlayer;
+
+    //camera speed
+    public float cameraSpeed = 0.125f;
+
+    //offset of the camera
+    public Vector3 offset;
+
+     void LateUpdate()
+    {
+        Vector3 desiredPosition = targetPlayer.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, cameraSpeed);
+        transform.position = smoothedPosition;
+
+        transform.LookAt(targetPlayer);
+    }
+
+
+
+}
