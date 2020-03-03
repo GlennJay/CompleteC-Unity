aaa1d38ae55baf1920ead0d2af2d3bf258040c38
GlennJay <glennwjason@gmail.com>
2020-03-03 16:30:54 -0500

    adding music and scene load

---
+++ Argon Assault/Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour
+{
+
+
+    //Scene activeScene;
+    // Start is called before the first frame update
+    void Start()
+    {
+         //activeScene = SceneManager.GetActiveScene();
+       // print(activeScene.buildIndex);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Invoke("LoadMainScene", 5f);
+    }
+
+
+   /*public void LoadMainScreen()
+    {
+        SceneManager.LoadScene(activeScene.buildIndex + 1);
+        Debug.Log(activeScene.buildIndex);
+
+    }*/
+}
