a15f7c0293ab1f362313327ebb2a53cd9fe591ac
GlennJay <glennwjason@gmail.com>
2020-03-10 15:46:33 -0400

    horizontal and vertical movement

--- Argon Assault/Assets/Scripts/Player.cs
+++ Argon Assault/Assets/Scripts/Player.cs
@@ -6,6 +6,14 @@ using UnityStandardAssets.CrossPlatformInput;
 public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    [Tooltip("In ms^-1")][SerializeField] float xSpeed = 4f;
+    [Tooltip("In meter")][SerializeField] float xRange = 7f;
+    [Tooltip("In ms^-1")] [SerializeField] float ySpeed = 4f;
+    [Tooltip("In meter")] [SerializeField] float yMinRange = -2;
+    [Tooltip("In meter")] [SerializeField] float yMaxRange = 3;
+
+
     void Start()
     {
         
@@ -15,8 +23,25 @@ public class Player : MonoBehaviour
     void Update()
     {
         //how far the stick moves
-        float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        print(horizontalThrow);
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+        //taking the local position of the ship and adding the offset to move the ship
+        float rawXPos = transform.localPosition.x + xOffset;
+        float XClampRange = Mathf.Clamp(rawXPos, -xRange, xRange);
+
+        //Implementing the y axis controlls
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffSet = yThrow * ySpeed * Time.deltaTime;
+
+        float rawYPos = transform.localPosition.y + yOffSet;
+        float yClampRnage = Mathf.Clamp(rawYPos, yMinRange, yMaxRange);
+
+        //moving the ship based off the xoffset and leaving the y and z to their local position of the parent camera object
+        transform.localPosition = new Vector3(XClampRange, yClampRnage,transform.localPosition.z) ;
+
+       
+        
+        
         
     }
 }
